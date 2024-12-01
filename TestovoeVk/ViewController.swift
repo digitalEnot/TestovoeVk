2dfd790baaa7160dd48337794cc02a760ba4ce33
digitalEnot <evgeni.novikk@icloud.com>
2024-12-02 00:09:24 +0300

    Initial Commit

---
+++ TestovoeVk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestovoeVk
+//
+//  Created by Evgeni Novik on 02.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
